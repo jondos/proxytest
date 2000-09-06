c88605b46a2c6427f32df412ef10fd23aefffb98
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2000-09-06 17:00:36 +0000

    Bugfixes and Signature enhancements...

--- CASignature.hpp
+++ CASignature.hpp
@@ -1,10 +1,13 @@
+#define SIGKEY_XML 1
 class CASignature
 	{
 		public:
 			CASignature();
 			~CASignature();
+			int setSignKey(char* buff,int len,int type);
 			int sign(unsigned char* in,int inlen,unsigned char* sig,unsigned int* siglen);
 			int getSignatureSize();
 		private:
 			DSA* dsa;
+			int parseSignKeyXML(char* buff,int len);
 	};
