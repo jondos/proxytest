7497f1bb48b747bb872a9ecb80993286f253a9fb
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2000-09-19 08:46:59 +0000

    Beginn documentation....

--- proxytest/trunk/CABase64.cpp
+++ proxytest/trunk/CABase64.cpp
@@ -51,13 +51,13 @@ SINT32 CABase64::encode(UINT8* in,UINT32 inlen,UINT8* out,UINT32 *outlen)
 				return E_SUCCESS;
 			}
 		if(inlen>*outlen)
-		 return E_UNKNOWN;
+			return E_UNKNOWN;
 		EVP_ENCODE_CTX oCTX;
 		EVP_EncodeInit(&oCTX);
 		UINT32 len;
 		*outlen=0;
 		EVP_EncodeUpdate(&oCTX,out,(int*)outlen,in,(int)inlen);
-		EVP_EncodeFinal(&oCTX,out+(*outlen),(int*)&len);
+		EVP_EncodeFinal(&oCTX,out+(*outlen),&len);
 		(*outlen)+=len;
 		return E_SUCCESS;
 	}
