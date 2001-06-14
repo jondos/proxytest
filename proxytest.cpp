ee0b7802c521402ab6be977967d33609732e6b08
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2001-06-14 08:28:26 +0000

    no message

--- proxytest.cpp
+++ proxytest.cpp
@@ -186,6 +186,21 @@ int main(int argc, const char* argv[])
 			#endif
 		#endif
 
+			UINT8 key[16];
+			UINT8 in[1024];
+			memset(key,0,16);
+			memset(in,0,1024);
+			CASymCipher* oSymCipher=new CASymCipher;
+			oSymCipher->setKeyAES(key);
+			unsigned long start=clock();
+			for(int i=0;i<1000;i++)
+				{
+					oSymCipher->encryptAES(in,in,1024);
+				}
+			start=clock()-start;
+			printf("Neede Time: %u\n",start);
+			printf("Out: %02X %02X %02X %02X %02X %02X %02X %02X\n",in[0],in[1],in[2],in[3],in[4],in[5],in[6],in[7]);
+			exit(0);
 	
 		options.parse(argc,argv);
 		if(options.getDaemon())
