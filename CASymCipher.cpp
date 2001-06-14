fdb8a82f6759361e4aabfab305b02aa2b4849d6e
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2001-06-14 08:49:40 +0000

    New AES (hopefully faster...)

--- CASymCipher.cpp
+++ CASymCipher.cpp
@@ -28,7 +28,6 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMA
 #include "StdAfx.h"
 //#include "aes/encrypt.cpp"
 #include "CASymCipher.hpp"
-#include "aes/encrypt.cpp"
 
 /*SINT32 CASymCipher::setEncryptionKey(UINT8* key)
 	{
