cf3f15d88c24baffc6ed68811bbe54bae7af4f03
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2001-11-22 12:16:20 +0000

    Added <Software> to Mix-Info

--- proxytest/trunk/CAUtil.hpp
+++ proxytest/trunk/CAUtil.hpp
@@ -33,6 +33,9 @@ UINT32 strtrim(UINT8*);
 
 SINT32 memtrim(UINT8* out,const UINT8* in,UINT32 len);
 
+char* strins(const char* src,UINT32 pos,const char* ins);
+char* strins(const char* src,const char * pos,const char* ins);
+
 SINT32 getcurrentTimeMillis(BIGNUM *bnTime); 
 
 SINT32 getRandom(UINT8* buff,UINT32 len);
