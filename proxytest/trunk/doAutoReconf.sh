e451fc91c656f6ab9b95a20bcfa4d9337ce5f411
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2003-04-10 14:11:42 +0000

    *** empty log message ***

--- proxytest/trunk/doAutoReconf.sh
+++ proxytest/trunk/doAutoReconf.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#Use this file if you have changed anything related to autoconf/automake
+#Please use only the newest version of autoconf/automake
 rm aclocal.m4 configure config.h.in
 autoreconf -i -f -v -W all
 cvs commit -m "Auto generate through doAutoReconf.sh" -f aclocal.m4 Makefile.in config.h.in configure popt/Makefile.in xml/Makefile.in aes/Makefile.in trio/Makefile.in
