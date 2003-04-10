776c9321af6c03732f725d7514a3f404e782becd
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2003-04-10 13:22:04 +0000

    *** empty log message ***

--- doAutoReconf.sh
+++ doAutoReconf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 autoreconf -i -f -v -W all
-cvs commit -m "Auto generate through doAutoReconf.sh" -f Makefile.in config.h.in configure popt/Makefile.in xml/Makefile.in aes/Makefile.in trio/Makefile.in
+cvs commit -m "Auto generate through doAutoReconf.sh" -f aclocal.m4 Makefile.in config.h.in configure popt/Makefile.in xml/Makefile.in aes/Makefile.in trio/Makefile.in
 cvs commit -m "Auto generate through doAutoReconf.sh"
