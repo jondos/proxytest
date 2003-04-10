c3459f442ab0029ec32e624700cbee81556729dd
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2003-04-10 13:24:25 +0000

    Auto generate through doAutoReconf.sh

--- doAutoReconf.sh
+++ doAutoReconf.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm aclocal.m4 configure config.h.in
 autoreconf -i -f -v -W all
 cvs commit -m "Auto generate through doAutoReconf.sh" -f aclocal.m4 Makefile.in config.h.in configure popt/Makefile.in xml/Makefile.in aes/Makefile.in trio/Makefile.in
 cvs commit -m "Auto generate through doAutoReconf.sh"
