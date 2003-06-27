865aa7bd778959a0c624c4bdea3c4b48b37e13dd
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2003-06-27 15:13:38 +0000

    Added online crime prevention

--- proxytest/trunk/CALastMix.cpp
+++ proxytest/trunk/CALastMix.cpp
@@ -651,8 +651,40 @@ ERR:
 
 #ifdef LOG_CRIME
 	bool CALastMix::checkCrime(UINT8* payLoad,UINT32 payLen)
-		{
-			return rand()<(RAND_MAX>>2);
+		{ //Lots of TODO!!!!
+			//DNS Lookup may block if Host does not exists!!!!!
+			UINT8* startOfUrl=(UINT8*)memchr(payLoad,32,payLen); //search for first space...
+			if(startOfUrl==NULL)
+				return false;
+			startOfUrl++;
+			UINT8* endOfUrl=(UINT8*)memchr(startOfUrl,32,payLen-(startOfUrl-payLoad)); //search for first space after start of URL 
+			if(endOfUrl==NULL)
+				return false;
+			UINT16 urlBuff[PAYLOAD_SIZE];
+			memset(urlBuff,0,PAYLOAD_SIZE);
+			UINT8* p;
+			UINT32 i=0;
+			for(p=startOfUrl;p<endOfUrl;p++)
+				urlBuff[i++]=*p;
+			XMLURL* url=new XMLURL(urlBuff);
+			char* strHost=XMLString::transcode(url->getHost());
+			if(strHost==NULL)
+				return false;
+			CASocketAddrINet oAddr;
+			if(oAddr.setAddr((UINT8*)strHost,0)==E_SUCCESS)
+				{
+					UINT8 ip[4];
+					oAddr.getIP(ip);
+					UINT8 ip_match[4];
+					ip_match[0]=141;
+					ip_match[1]=76;
+					ip_match[2]=46;
+					ip_match[3]=1;
+					delete[] strHost;
+					return (memcmp(ip,ip_match,4)==0);
+				}
+			delete[] strHost;
+			return false;			
 		}
 #endif
 
