7f3c5cbdfc4c2241f2af20d91d72be66e8bbd028
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2003-08-20 16:44:30 +0000

    Start adding pool to middlemix

--- CAPool.cpp
+++ CAPool.cpp
@@ -4,11 +4,13 @@
 
 CAPool::CAPool(UINT32 poolsize)
 	{
+		m_uPoolSize=poolsize;
 		m_pPoolList=new tPoolListEntry;
 		getRandom(m_pPoolList->mixpacket.data,DATA_SIZE);
 		m_pPoolList->mixpacket.flags=CHANNEL_CLOSE;
 		m_pPoolList->mixpacket.channel=0;
 		m_pPoolList->next=NULL;
+		m_pLastEntry=m_pPoolList;
 		for(UINT32 i=1;i<poolsize;i++)
 			{
 				tPoolListEntry* tmpEntry=new tPoolListEntry;
@@ -18,6 +20,7 @@ CAPool::CAPool(UINT32 poolsize)
 				tmpEntry=m_pPoolList;
 				m_pPoolList=tmpEntry;
 			}
+		m_pEntry=new tPoolListEntry;	
 	}
 	
 CAPool::~CAPool()
@@ -29,10 +32,55 @@ CAPool::~CAPool()
 				m_pPoolList=m_pPoolList->next;
 				delete tmpEntry;
 			}
+		delete m_pEntry;	
 	}
 	
 SINT32 CAPool::pool(MIXPACKET* pMixPacket)
 	{
+		UINT32 v;
+		getRandom(&v);
+		v=v%m_uPoolSize;
+		tPoolListEntry* tmpEntry=m_pPoolList;
+		while(v>0)
+			{
+				tmpEntry=tmpEntry->next;
+				v--;
+			}
+		HCHANNEL id=tmpEntry->mixpacket.channel;
+		tPoolListEntry* pEntryOut=tmpEntry;
+		tmpEntry=m_pPoolList;
+		tPoolListEntry* pPrevEntry=NULL;		
+		while(tmpEntry!=pEntryOut)
+			{
+				if(tmpEntry->mixpacket.channel==id)
+					{
+						pEntryOut=tmpEntry;
+						break;
+					}
+				pPrevEntry=tmpEntry;
+				tmpEntry=tmpEntry->next;	
+			}
+
+		if(pEntryOut==m_pPoolList) //first element to remove)
+			{
+				memcpy(&m_pEntry->mixpacket,pMixPacket,MIXPACKET_SIZE);
+				memcpy(pMixPacket,&pEntryOut->mixpacket,MIXPACKET_SIZE);
+				m_pPoolList=pEntryOut->next;
+				m_pLastEntry->next=m_pEntry;
+				m_pLastEntry=m_pEntry;
+				m_pLastEntry->next=NULL;
+				m_pEntry=pEntryOut;
+			}
+		else
+			{
+				memcpy(&m_pEntry->mixpacket,pMixPacket,MIXPACKET_SIZE);
+				memcpy(pMixPacket,&pEntryOut->mixpacket,MIXPACKET_SIZE);
+				m_pLastEntry->next=m_pEntry;
+				m_pLastEntry=m_pEntry;
+				m_pLastEntry->next=NULL;
+				pPrevEntry->next=pEntryOut->next;
+				m_pEntry=pEntryOut;		
+			}		
 		return E_SUCCESS;
 	}
 		
