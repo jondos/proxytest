9765d9cfe01fa6ea5cd5b86cc92d43e3d6dc18a4
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2004-06-03 14:43:07 +0000

    Some small BugFixes

--- TypeA/CAFirstMixA.cpp
+++ TypeA/CAFirstMixA.cpp
@@ -31,6 +31,7 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMA
 #include "../CASingleSocketGroup.hpp"
 #include "../CAInfoService.hpp"
 #include "../CAPool.hpp"
+#include ../CACmdLnOptions.hpp"
 
 extern CACmdLnOptions options;
 
--- TypeA/CALastMixA.cpp
+++ TypeA/CALastMixA.cpp
@@ -31,6 +31,8 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMA
 #include "../CASingleSocketGroup.hpp"
 #include "../CAPool.hpp"
 
+extern CACmdLnOptions options;
+
 SINT32 CALastMixA::loop()
 	{
 #ifndef NEW_MIX_TYPE
