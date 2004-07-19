50313f929888ddf050ea0f046b778b3440ecc9bc
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2004-07-19 12:19:39 +0000

    BugFxies

--- CATimedQueue.hpp
+++ CATimedQueue.hpp
@@ -34,14 +34,33 @@ class CATimedQueue
 	public:
 		//CATimedQueue();
 		CATimedQueue(UINT32 expectedSize);
-		SINT32 add(const void* pbuff,UINT32 size,const UINT64& timestamp);
-		SINT32 getOrWait(UINT8* pbuff,UINT32* psize,UINT64& timestamp);
-		SINT32 getOrWait(UINT8* pbuff,UINT32* psize,UINT64& timestamp,UINT32 ms_TimeOut);
+		SINT32 add(const void* pbuff,UINT32 size,const UINT64& timestamp)
+			{
+				m_pQueue->add(pbuff,size);
+				return m_pQueue->add(&timestamp,sizeof(timestamp));
+			}
+
+		SINT32 getOrWait(UINT8* pbuff,UINT32* psize,UINT64& timestamp)
+			{
+				m_pQueue->getOrWait(pbuff,psize);
+				UINT32 len=sizeof(timestamp);
+				return m_pQueue->getOrWait((UINT8*)&timestamp,&len);
+			}
+
+		SINT32 getOrWait(UINT8* pbuff,UINT32* psize,UINT64& timestamp,UINT32 ms_TimeOut)
+			{
+				SINT32 ret=m_pQueue->getOrWait(pbuff,psize,ms_TimeOut);
+				if(ret!=E_SUCCESS)
+					return ret;
+				UINT32 len=sizeof(timestamp);
+				return m_pQueue->getOrWait((UINT8*)&timestamp,&len);
+			}
+
 		SINT32 get(UINT8* pbuff,UINT32* psize,UINT64& timestamp)
 			{
 				UINT32 len=sizeof(UINT64);
 				m_pQueue->get(pbuff,psize);
-				return m_pQueue->get((UINT8*)timestamp,&len);
+				return m_pQueue->get((UINT8*)&timestamp,&len);
 			}
 
 		SINT32 get(UINT8* pbuff,UINT32* psize)
