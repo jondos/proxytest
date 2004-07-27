4a33f5877e3c286dc80a4568f97e006e1f1f9062
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2004-07-27 14:46:59 +0000

    BugFixes

--- TypeA/CALastMixA.cpp
+++ TypeA/CALastMixA.cpp
@@ -32,7 +32,7 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMA
 #include "../CAPool.hpp"
 #include "../CACmdLnOptions.hpp"
 #ifdef HAVE_EPOLL
-#include "CASocketGroupEpoll.hpp"
+#include "../CASocketGroupEpoll.hpp"
 #endif
 
 extern CACmdLnOptions options;
