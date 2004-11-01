f0b619cc1eb02d42ae63e5fdfde17363cb2c7748
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2004-11-01 15:08:48 +0000

    *** empty log message ***

--- proxytest/trunk/CAAccountingDBInterface.hpp
+++ proxytest/trunk/CAAccountingDBInterface.hpp
@@ -28,7 +28,6 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMA
 #ifndef __CAACCOUNTINGDBINTERFACE__
 #define __CAACCOUNTINGDBINTERFACE__
 
-#include "StdAfx.h"
 #include "CAQueue.hpp"
 #include "CAXMLCostConfirmation.hpp"
 
