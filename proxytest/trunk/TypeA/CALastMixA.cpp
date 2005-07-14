76292e8b41e2f7bd2e83bc4539730935edc9b7c5
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2005-07-14 20:05:32 +0000

    *** empty log message ***

--- proxytest/trunk/TypeA/CALastMixA.cpp
+++ proxytest/trunk/TypeA/CALastMixA.cpp
@@ -475,14 +475,17 @@ SINT32 CALastMixA::loop()
 														#endif
 														pMixPacket->channel=pChannelListEntry->channelIn;
 														pMixPacket->flags=CHANNEL_DATA;
-														pMixPacket->payload.len=htons((UINT16)ret);
 														pMixPacket->payload.type=0;
 														#ifdef NEW_FLOW_CONTROL
 														if(pChannelListEntry->sendmeCounter==FLOW_CONTROL_SENDME_SOFT_LIMIT)
 															{
-																pMixPacket->payload.len|=NEW_FLOW_CONTROL_FLAG;
+																pMixPacket->payload.len=htons((UINT16)ret|NEW_FLOW_CONTROL_FLAG);
 																CAMsg::printMsg(LOG_DEBUG,"Send sendme request\n");
 															}
+														else
+															pMixPacket->payload.len=htons((UINT16)ret);															
+														#else
+															pMixPacket->payload.len=htons((UINT16)ret);
 														#endif
 														pChannelListEntry->pCipher->crypt2(pMixPacket->data,pMixPacket->data,DATA_SIZE);
 														#ifdef LOG_PACKET_TIMES
