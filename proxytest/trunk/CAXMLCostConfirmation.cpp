bc213da23be4a8046181bdcfc28f7d449bff3dbc
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2005-10-31 10:42:15 +0000

    *** empty log message ***

--- proxytest/trunk/CAXMLCostConfirmation.cpp
+++ proxytest/trunk/CAXMLCostConfirmation.cpp
@@ -3,56 +3,49 @@
 #include "CAUtil.hpp"
 #include "CAMsg.hpp"
 
-UINT8* CAXMLCostConfirmation::ms_pStrElemName=NULL;
+const UINT8* const CAXMLCostConfirmation::ms_pStrElemName=(UINT8*)"CC";
 
 CAXMLCostConfirmation::CAXMLCostConfirmation(UINT8 * strXmlData) 
-{
-	// parse XML
-	MemBufInputSource oInput( strXmlData, strlen((char*)strXmlData), "XMLCostConfirmation" );
-	DOMParser oParser;
-	oParser.parse( oInput );
-	m_domDocument = oParser.getDocument();
-	m_pStrAiName = NULL;
-	setValues();
-}
-
-
-CAXMLCostConfirmation::CAXMLCostConfirmation(DOM_Element &elemRoot)
-{
-	m_pStrAiName = NULL;
-	m_domDocument=DOM_Document::createDocument();
-	m_domDocument.appendChild(m_domDocument.importNode(elemRoot,true));
-	setValues();
-}
-
-UINT8 * CAXMLCostConfirmation::getXMLElementName()
-{
-	if(!CAXMLCostConfirmation::ms_pStrElemName)
 	{
-		UINT8* elemName = (UINT8*)"CC";
-		CAXMLCostConfirmation::ms_pStrElemName = new UINT8[strlen((char *)elemName)+1];
-		strcpy((char*)CAXMLCostConfirmation::ms_pStrElemName, (char*)elemName);
+		// parse XML
+		MemBufInputSource oInput( strXmlData, strlen((char*)strXmlData), "XMLCostConfirmation" );
+		DOMParser oParser;
+		oParser.parse( oInput );
+		m_domDocument = oParser.getDocument();
+		m_pStrAiName = NULL;
+		m_pStrPIID = NULL;
+		setValues();
 	}
-	return CAXMLCostConfirmation::ms_pStrElemName;
-}
+
+
+	CAXMLCostConfirmation::CAXMLCostConfirmation(DOM_Element &elemRoot)
+		{
+			m_pStrAiName = NULL;
+			m_pStrPIID = NULL;
+			m_domDocument=DOM_Document::createDocument();
+			m_domDocument.appendChild(m_domDocument.importNode(elemRoot,true));
+			setValues();
+	}
 
 CAXMLCostConfirmation::~CAXMLCostConfirmation()
 	{
 		if(m_pStrAiName!=NULL)
 			delete[] m_pStrAiName;
+		if(m_pStrPIID!=NULL)
+			delete[] m_pStrPIID;
 		m_domDocument=NULL;
 	}
 
 
 SINT32 CAXMLCostConfirmation::setValues()
 	{
+		if(m_domDocument==NULL)
+			return E_UNKNOWN;
 		DOM_Element elemRoot=m_domDocument.getDocumentElement();
 		DOM_Element elem; 
-		UINT8 strGeneral[128];
-		UINT32 strGeneralLen = 128;
 	
 		char * strTagname = elemRoot.getTagName().transcode();
-		if( (strcmp((char *)strTagname, (char *)getXMLElementName())!=0) )
+		if( (strcmp((char *)strTagname, (char *)ms_pStrElemName)!=0) )
 			{
 				delete[] strTagname;
 				return E_UNKNOWN;
@@ -61,12 +54,17 @@ SINT32 CAXMLCostConfirmation::setValues()
 	
 		// parse AI Name
 		getDOMChildByName(elemRoot, (UINT8*)"AiID", elem, false);
-		getDOMElementValue(elem, strGeneral, &strGeneralLen);
 		if(m_pStrAiName!=NULL) 
 			delete[] m_pStrAiName;
-		m_pStrAiName = new UINT8[strGeneralLen+1];
-		strcpy((char*)m_pStrAiName, (char*)strGeneral);
-	
+		m_pStrAiName=NULL;
+		UINT8 strGeneral[256];
+		UINT32 strGeneralLen = 256;
+		if(getDOMElementValue(elem, strGeneral, &strGeneralLen)==E_SUCCESS)
+			{
+				m_pStrAiName = new UINT8[strGeneralLen+1];
+				memcpy(m_pStrAiName, strGeneral,strGeneralLen+1);
+			}
+			
 		// parse accountnumber
 		getDOMChildByName(elemRoot, (UINT8*)"AccountNumber", elem, false);
 		getDOMElementValue(elem, m_lAccountNumber);
@@ -74,6 +72,17 @@ SINT32 CAXMLCostConfirmation::setValues()
 		// parse transferredBytes
 		getDOMChildByName(elemRoot, (UINT8*)"TransferredBytes", elem, false);
 		getDOMElementValue(elem, m_lTransferredBytes);
-	
+		
+		// parse PIID
+		if(m_pStrPIID!=NULL) 
+			delete[] m_pStrPIID;
+		m_pStrPIID=NULL;	
+		strGeneralLen=255;
+		getDOMChildByName(elemRoot, (UINT8*)"PIID", elem, false);
+		if(getDOMElementValue(elem, strGeneral, &strGeneralLen)==E_SUCCESS)
+			{
+				m_pStrPIID = new UINT8[strGeneralLen+1];
+				memcpy(m_pStrPIID, strGeneral,strGeneralLen+1);
+			}	
 		return E_SUCCESS;
 	}
