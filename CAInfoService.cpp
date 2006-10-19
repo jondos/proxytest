aea205ed367dd35621b337739c99c5ae1a89eefb
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2006-10-19 14:28:08 +0000

    no message

--- CAInfoService.cpp
+++ CAInfoService.cpp
@@ -50,7 +50,7 @@ const UINT64 CAInfoService::MINUTE = 60;
 const UINT64 CAInfoService::SEND_CASCADE_INFO_WAIT = MINUTE * 10;
 const UINT64 CAInfoService::SEND_MIX_INFO_WAIT = MINUTE * 10;
 const UINT64 CAInfoService::SEND_STATUS_INFO_WAIT = MINUTE;
-const UINT32 CAInfoService::SEND_INFO_TIMEOUT = 10; // 10 seconds timeout
+const UINT32 CAInfoService::SEND_INFO_TIMEOUT = 10000; // 10 seconds timeout
 
 static THREAD_RETURN InfoLoop(void *p)
 	{
