d9b7c50adc61059a72d67c76c9470b920a3af441
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-04-20 12:21:08 +0000

    *** empty log message ***

---
+++ mixtables.sql
@@ -0,0 +1,6 @@
+drop table costconfirmations;
+drop table prepaidamounts;
+drop table accountstatus;
+create table costconfirmations(accountnumber bigserial,bytes bigint,xmlcc varchar(2000),settled integer,cascade varchar(200),primary key(accountnumber,cascade));
+create table prepaidamounts(accountnumber bigint unique not null,prepaidbytes integer,cascade varchar(200),primary key(accountnumber, cascade));
+create table accountstatus(accountnumber bigint, statuscode integer);	
