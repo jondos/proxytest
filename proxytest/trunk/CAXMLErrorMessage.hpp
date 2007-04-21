13aa04a976f82f7d95743289e69e083d37fbc9bf
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-04-21 08:39:11 +0000

    *** empty log message ***

--- proxytest/trunk/CAXMLErrorMessage.hpp
+++ proxytest/trunk/CAXMLErrorMessage.hpp
@@ -59,7 +59,7 @@ class CAXMLErrorMessage : public CAAbstractXMLEncodable
 			static const UINT32 ERR_INSUFFICIENT_BALANCE = 13;
 			static const UINT32 ERR_NO_FLATRATE_OFFERED = 14;
 			static const UINT32 ERR_INVALID_CODE = 15;
-			static const UINT32 ERR_INVALID_CC = 16;
+			static const UINT32 ERR_OUTDATED_CC = 16;
 			static const UINT32 ERR_INVALID_PRICE_CERT = 17;
 			
 		
