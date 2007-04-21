4c2a36633180056616a868c75476cf5459e309a3
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-04-21 09:11:03 +0000

    *** empty log message ***

--- CAAccountingInstance.cpp
+++ CAAccountingInstance.cpp
@@ -1096,12 +1096,12 @@ SINT32 CAAccountingInstance::cleanupTableEntry( fmHashTableEntry *pHashEntry )
 			if (ms_pInstance->m_settleHashtable)
 			{
 				ms_pInstance->m_settleHashtable->getMutex().lock();				
-				entry = (AccountHashEntry*)ms_pInstance->m_settleHashtable->remove(&(pAccInfo->accountNumber));
+				entry = (AccountHashEntry*)ms_pInstance->m_settleHashtable->remove(&(pAccInfo->accountNumber));										
+				ms_pInstance->m_settleHashtable->getMutex().unlock();	
 				if (entry)
 				{
 					delete entry;				
-				}						
-				ms_pInstance->m_settleHashtable->getMutex().unlock();	
+				}
 			}
 			
 			//free memory of pAccInfo
