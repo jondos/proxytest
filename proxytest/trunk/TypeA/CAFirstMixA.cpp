3bdc1d5a07891dceee6a3d0b64ce164c9c05b4ea
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-04-24 17:37:45 +0000

    *** empty log message ***

--- proxytest/trunk/TypeA/CAFirstMixA.cpp
+++ proxytest/trunk/TypeA/CAFirstMixA.cpp
@@ -516,8 +516,7 @@ NEXT_USER:
 													//if(pfmHashEntry->bCountPacket)
 														{
 															// count packet for payment
-															ret = CAAccountingInstance::handleJapPacket(pfmHashEntry, !(pfmHashEntry->bCountPacket), true);
-															if (ret == 3)
+															if (CAAccountingInstance::handleJapPacket(pfmHashEntry, !(pfmHashEntry->bCountPacket), true) == 3)
 															{
 																// this jap is evil! terminate connection and add IP to blacklist
 																CAMsg::printMsg(LOG_DEBUG, "CAFirstMixA: Detected evil Jap.. closing connection! Removing IP..\n", ret);
@@ -545,10 +544,6 @@ NEXT_USER:
 																decUsers();
 																goto NEXT_USER_WRITING;
 															}
-															else if (ret = 2)
-															{
-																goto NEXT_USER_WRITING;
-															}															
 														}
 												#endif
 												#ifdef DELAY_USERS
