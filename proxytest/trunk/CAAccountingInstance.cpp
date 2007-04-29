a96895a655609d5406e823ff55315a5c2a6f2e8e
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-04-29 14:00:10 +0000

    *** empty log message ***

--- proxytest/trunk/CAAccountingInstance.cpp
+++ proxytest/trunk/CAAccountingInstance.cpp
@@ -758,10 +758,10 @@ SINT32 CAAccountingInstance::processJapMessage(fmHashTableEntry * pHashEntry,con
 		}
 		
 		CAMsg::printMsg( LOG_DEBUG, "Creating queue item\n");
-		
-		pDoc = new DOM_Document;
+
+		pDoc = new DOM_Document(a_DomDoc);
 		CAMsg::printMsg( LOG_DEBUG, "Importing node for queue item\n");
-		pDoc->appendChild(pDoc->importNode(root, true));
+		//pDoc->appendChild(pDoc->importNode(root, true));
 		CAMsg::printMsg( LOG_DEBUG, "Node for queue item imported\n");
 		pItem = new aiQueueItem;
 		pItem->pDomDoc = pDoc;
