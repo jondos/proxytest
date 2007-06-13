369711eb217fdc7baa3f424a98f31d8c88281374
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-06-13 00:36:01 +0000

    *** empty log message ***

--- CASocket.cpp
+++ CASocket.cpp
@@ -84,6 +84,7 @@ SINT32 CASocket::create(UINT32 a_category, bool a_bShowTypicalError)
 ///@todo Not thread safe!
 SINT32 CASocket::create(UINT32 a_category, int type, bool a_bShowTypicalError)
 	{
+		ms_mutexCount.lock();
 		if (ms_categoryCounts == NULL)
 		{	
 			ms_categoryCounts = new UINT32[12];
@@ -92,6 +93,7 @@ SINT32 CASocket::create(UINT32 a_category, int type, bool a_bShowTypicalError)
 				ms_categoryCounts[i] = 0;
 			}
 		}		
+		ms_mutexCount.unlock();
 		
 		if(m_bSocketIsClosed)
 		{
@@ -129,12 +131,14 @@ SINT32 CASocket::create(UINT32 a_category, int type, bool a_bShowTypicalError)
 		m_csClose.lock();
 		if(!m_bIsReservedSocket)
 		{
-			m_u32NormalSocketsOpen++;
-			ms_categoryCounts[m_category]++;
+			ms_mutexCount.lock();
+			m_u32NormalSocketsOpen++;			
+			ms_categoryCounts[m_category]++;			
 			if (ms_categoryCounts[m_category] % 50 == 0)
 			{
 				CAMsg::printMsg(LOG_CRIT,"Nr. of sockets of category %d: '%d'\n", m_category , ms_categoryCounts[m_category]);
 			}
+			ms_mutexCount.unlock();
 		}
 		m_csClose.unlock();
 		return E_SUCCESS;
@@ -210,8 +214,10 @@ SINT32 CASocket::accept(CASocket &s)
 				return E_UNKNOWN;
 			}
 		m_csClose.lock();
+		ms_mutexCount.lock();
 		m_u32NormalSocketsOpen++;
 		ms_categoryCounts[m_category]++;
+		ms_mutexCount.unlock();
 		m_csClose.unlock();
 		s.m_bSocketIsClosed=false;
 		return E_SUCCESS;
@@ -338,8 +344,10 @@ SINT32 CASocket::close()
 				::closesocket(m_Socket);
 			if(!m_bIsReservedSocket)
 			{
+				ms_mutexCount.lock();
 				m_u32NormalSocketsOpen--;
 				ms_categoryCounts[m_category]--;
+				ms_mutexCount.unlock();
 			}
 				m_bSocketIsClosed=true;
 				m_closeMode=0;
