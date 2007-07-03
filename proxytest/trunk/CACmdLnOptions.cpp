05817c0083a11e47ff3a1d3ad2daf05e0eddac1c
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-07-03 13:00:16 +0000

    *** empty log message ***

--- proxytest/trunk/CACmdLnOptions.cpp
+++ proxytest/trunk/CACmdLnOptions.cpp
@@ -2226,7 +2226,7 @@ SKIP_NEXT_MIX:
 			DOM_Element elemProxies=m_docMixInfo.createElement("Proxies");
 			if (m_bSocksSupport)
 			{
-				setDOMElementAttribute(elemProxies, "sock5Support", "true");
+				setDOMElementAttribute(elemProxies, "sock5Support", (UINT8*)"true");
 			}
 			DOM_Element elemProxy=m_docMixInfo.createElement("Proxy");
 			DOM_Element elemVisAddresses=m_docMixInfo.createElement("VisibleAddresses");
