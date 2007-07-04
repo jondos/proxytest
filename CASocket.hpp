bd2317dfcd4ccb7038e9f0cdfc04467fcbe69cb2
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-07-04 13:04:13 +0000

    *** empty log message ***

--- CASocket.hpp
+++ CASocket.hpp
@@ -59,7 +59,6 @@ class CASocket:public CAClientSocket
 			SINT32 sendFullyTimeOut(const UINT8* buff,UINT32 len, UINT32 msTimeOut, UINT32 msTimeOutSingleSend);
 			SINT32 sendTimeOut(const UINT8* buff,UINT32 len,UINT32 msTimeOut);
 			SINT32 receive(UINT8* buff,UINT32 len);
-			SINT32 receiveFullyT(UINT8* buff,UINT32 len,UINT32 msTimeOut);
 			/** Returns the number of the Socket used. Which will be always the same number,
 				* even after close(), until the Socket
 				* is recreated using create()
