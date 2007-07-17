dc5a9cd439009d82660cebdbbf6a262153ffbd84
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-07-17 18:43:46 +0000

    *** empty log message ***

--- CAAccountingInstance.cpp
+++ CAAccountingInstance.cpp
@@ -328,7 +328,7 @@ SINT32 CAAccountingInstance::handleJapPacket_internal(fmHashTableEntry *pHashEnt
 		UINT8 tmp[32];
 		print64(tmp, pAccInfo->transferredBytes);
 		UINT8 tmp2[32];
-		print64(tmp2, pAccInfo->transferredBytes);
+		print64(tmp2, pAccInfo->confirmedBytes);
 		CAMsg::printMsg(LOG_DEBUG, "CAAccountingInstance: Transferred Bytes: %s Confirmed Bytes: %s\n", tmp, tmp2);	
 		
 		// do the following tests after a lot of Mix packets only (gain speed...)
