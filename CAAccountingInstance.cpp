ad0e0af54604eac174fb7d20d802695f33f18c14
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-07-23 19:50:21 +0000

    *** empty log message ***

--- CAAccountingInstance.cpp
+++ CAAccountingInstance.cpp
@@ -326,6 +326,12 @@ SINT32 CAAccountingInstance::handleJapPacket_internal(fmHashTableEntry *pHashEnt
 			pAccInfo->sessionPackets++;
 		}		
 		
+			UINT8 tmp[32];
+		print64(tmp,pAccInfo->transferredBytes);
+		CAMsg::printMsg(LOG_DEBUG, "CAAccountingInstance1: Transferred bytes:%s\n", (tmp - m_countTransferred));	
+		
+		print64(tmp,pAccInfo->confirmedBytes);
+		CAMsg::printMsg(LOG_DEBUG, "CAAccountingInstance2: Confirmed bytes:  %s\n", tmp);	
 		
 		
 		// do the following tests after a lot of Mix packets only (gain speed...)
@@ -337,13 +343,7 @@ SINT32 CAAccountingInstance::handleJapPacket_internal(fmHashTableEntry *pHashEnt
 			return HANDLE_PACKET_CONNECTION_UNCHECKED;
 		}
 		
-		UINT8 tmp[32];
-		print64(tmp,pAccInfo->transferredBytes);
-		CAMsg::printMsg(LOG_DEBUG, "CAAccountingInstance1: Transferred bytes:%s\n", tmp);	
-		
-		print64(tmp,pAccInfo->confirmedBytes);
-		CAMsg::printMsg(LOG_DEBUG, "CAAccountingInstance2: Confirmed bytes:  %s\n", tmp);	
-		
+	
 		//CAMsg::printMsg( LOG_DEBUG, "Checking after %d session packets...\n", pAccInfo->sessionPackets);
 		
 		/** @todo We need this trick so that the program does not freeze with active AI ThreadPool!!!! */
@@ -1057,6 +1057,7 @@ void CAAccountingInstance::handleAccountCertificate_internal(tAiAccountingInfo* 
 		m_dbInterface->getCostConfirmation(pAccInfo->accountNumber, m_currentCascade, &pCC);
 		if(pCC!=NULL)
 		{
+			m_countTransferred = pCC->getTransferredBytes();
 			pAccInfo->transferredBytes += pCC->getTransferredBytes();
 			pAccInfo->confirmedBytes = pCC->getTransferredBytes();
 			#ifdef DEBUG
@@ -1629,7 +1630,7 @@ SINT32 CAAccountingInstance::initTableEntry( fmHashTableEntry * pHashEntry )
 {
 	INIT_STACK;
 	BEGIN_STACK("CAAccountingInstance::initTableEntry");
-	
+	m_countTransferred = 0;
 	//ms_pInstance->m_Mutex.lock();
 	
 	if (pHashEntry == NULL)
@@ -1725,7 +1726,7 @@ SINT32 CAAccountingInstance::cleanupTableEntry( fmHashTableEntry *pHashEntry )
 								 */
 								CAMsg::printMsg(LOG_ERR, 
 									"PrepaidBytes of %d for account %s are higher than prepaid interval! "
-									"The client did not behave according to specification."
+									"The client did not behave according to specification. "
 									"Deleting %d bytes...\n", prepaidBytes, tmp, prepaidBytes - prepaidInterval);
 								
 								prepaidBytes = prepaidInterval;
