53a407099f1586bb3c6362d0aa59f25137da9f7f
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-07-23 15:55:35 +0000

    *** empty log message ***

--- CAPriceInfo.cpp
+++ CAPriceInfo.cpp
@@ -29,11 +29,6 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMA
 #include "CAPriceInfo.hpp"
 
 
-CAPriceInfo::CAPriceInfo()
-{
-	m_pStrPriceCertHash = NULL;
-	m_pStrMixId = NULL;
-}
 
 CAPriceInfo::CAPriceInfo(UINT8* strMixId, UINT8* strPriceCertHash, UINT32 a_postition)
 {
