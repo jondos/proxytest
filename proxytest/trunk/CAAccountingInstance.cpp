ac0ce2ce0384ce4be6d640d052b9c1984a7b2df8
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-07-23 16:37:37 +0000

    *** empty log message ***

--- proxytest/trunk/CAAccountingInstance.cpp
+++ proxytest/trunk/CAAccountingInstance.cpp
@@ -1340,7 +1340,6 @@ void CAAccountingInstance::handleChallengeResponse_internal(tAiAccountingInfo* p
 		}
 		else
 		{
-CAMsg::printMsg(LOG_INFO, "CAAccountingInstance:");
 			UINT8 tmp2[32];
 			print64(tmp2, pAccInfo->transferredBytes);
 			CAMsg::printMsg(LOG_ERR, "CAAccountingInstance: Transfered bytes of %s for account %s are lower than prepaid amount!\n",tmp2, tmp);
