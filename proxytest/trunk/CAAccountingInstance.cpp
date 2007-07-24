76ea9af7c8a680f32c346147fed05aa9a797aba9
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2007-07-24 15:48:09 +0000

    *** empty log message ***

--- proxytest/trunk/CAAccountingInstance.cpp
+++ proxytest/trunk/CAAccountingInstance.cpp
@@ -326,7 +326,7 @@ SINT32 CAAccountingInstance::handleJapPacket_internal(fmHashTableEntry *pHashEnt
 			pAccInfo->sessionPackets++;
 		}		
 		
-		
+		/*
 		UINT8 tmp[32];
 		//print64(tmp,pAccInfo->transferredBytes - m_countTransferred);
 		//CAMsg::printMsg(LOG_DEBUG, "CAAccountingInstance1: Transferred bytes:%s\n", tmp);	
@@ -336,7 +336,7 @@ SINT32 CAAccountingInstance::handleJapPacket_internal(fmHashTableEntry *pHashEnt
 		print64(tmp,pAccInfo->confirmedBytes);
 		CAMsg::printMsg(LOG_DEBUG, "CAAccountingInstance3: Confirmed bytes:  %s\n", tmp);	
 		CAMsg::printMsg(LOG_DEBUG, "CAAccountingInstance3: Prepaid bytes:    %d\n", getPrepaidBytes(pAccInfo));	
-			
+		*/
 		
 		
 		
@@ -1763,7 +1763,7 @@ SINT32 CAAccountingInstance::cleanupTableEntry( fmHashTableEntry *pHashEntry )
 				if (loginEntry)
 				{
 					// test: delete CC!!!
-					ms_pInstance->m_dbInterface->deleteCC(pAccInfo->accountNumber, ms_pInstance->m_currentCascade);
+					//ms_pInstance->m_dbInterface->deleteCC(pAccInfo->accountNumber, ms_pInstance->m_currentCascade);
 					
 					if (pAccInfo->userID == loginEntry->userID)// &&
 						//!(pAccInfo->authFlags & AUTH_WAITING_FOR_FIRST_SETTLED_CC))
