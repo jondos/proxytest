2dcffadd37a780e573fd2c26eeff0d5ea34f08ac
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2008-07-13 09:34:57 +0000

    *** empty log message ***

--- CAMuxSocket.cpp
+++ CAMuxSocket.cpp
@@ -183,12 +183,19 @@ SINT32 CAMuxSocket::prepareForSend(MIXPACKET *pinoutPacket)
 	*/
 SINT32 CAMuxSocket::receive(MIXPACKET* pPacket)
 	{
-		m_csReceive.lock();
+		UINT32 retLock = m_csReceive.lock();
+		if (retLock != E_SUCCESS)
+		{
+			CAMsg::printMsg(LOG_CRIT,
+				"Could not lock MuxSocket receive method! Error code: %d\n", retLock);
+			return E_UNKNOWN;
+		}
+		
 		if(m_Socket.receiveFully((UINT8*)pPacket,MIXPACKET_SIZE)!=E_SUCCESS)
-			{
-				m_csReceive.unlock();
-				return SOCKET_ERROR;
-			}
+		{
+			m_csReceive.unlock();
+			return SOCKET_ERROR;
+		}
 		if(m_bIsCrypted)
     	m_oCipherIn.crypt1((UINT8*)pPacket,(UINT8*)pPacket,16);
 		pPacket->channel=ntohl(pPacket->channel);
@@ -210,7 +217,7 @@ SINT32 CAMuxSocket::receive(MIXPACKET* pPacket,UINT32 msTimeout)
 		if (retLock != E_SUCCESS)
 		{
 			CAMsg::printMsg(LOG_CRIT,
-				"Could not lock MuxSocket receive method! Error code: %d\n", retLock);
+				"Could not lock MuxSocket timed receive method! Error code: %d\n", retLock);
 			return E_UNKNOWN;
 		}
 		SINT32 len=MIXPACKET_SIZE-m_aktBuffPos;
