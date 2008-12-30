ffd650bf45252e33f58d813d12b4b974e594a3f7
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2008-12-30 20:18:56 +0000

    Auto generate through doAutoReconf.sh

--- proxytest/trunk/doAutoReconf.sh
+++ proxytest/trunk/doAutoReconf.sh
@@ -3,5 +3,5 @@
 #Please use only the newest version of autoconf/automake
 rm missing aclocal.m4 configure config.h.in
 autoreconf -i -f -v -W all
-#cvs commit -m "Auto generate through doAutoReconf.sh" -f missing aclocal.m4 Makefile.in config.h.in configure popt/Makefile.in xml/Makefile.in trio/Makefile.in tre/Makefile.in TypeA/Makefile.in TypeB/Makefile.in
-#cvs commit -m "Auto generate through doAutoReconf.sh"
+cvs commit -m "Auto generate through doAutoReconf.sh" -f missing aclocal.m4 Makefile.in config.h.in configure popt/Makefile.in xml/Makefile.in trio/Makefile.in tre/Makefile.in TypeA/Makefile.in TypeB/Makefile.in
+cvs commit -m "Auto generate through doAutoReconf.sh"
