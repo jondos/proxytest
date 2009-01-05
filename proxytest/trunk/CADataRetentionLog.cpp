5d8d08d881aff37e0165e0604c1ee212261a8edd
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2009-01-05 14:18:12 +0000

    Many data retention related bug fixes

--- proxytest/trunk/CADataRetentionLog.cpp
+++ proxytest/trunk/CADataRetentionLog.cpp
@@ -6,6 +6,13 @@ CADataRetentionLog::CADataRetentionLog()
 	{
 		m_pLogFile=NULL;
 		m_pPublicEncryptionKey=NULL;
+		m_strLogDir=NULL;
+	}
+
+CADataRetentionLog::~CADataRetentionLog()
+	{
+		closeLog();
+		delete[] m_strLogDir;
 	}
 
 SINT32 CADataRetentionLog::setPublicEncryptionKey(CAASymCipher* pPublicKey)
@@ -16,12 +23,47 @@ SINT32 CADataRetentionLog::setPublicEncryptionKey(CAASymCipher* pPublicKey)
 
 SINT32 CADataRetentionLog::setLogDir(UINT8* strDir)
 	{
+		UINT32 l=strlen((char*)strDir);
+		m_strLogDir=new UINT8[l+1];
+		memcpy(m_strLogDir,strDir,l);
+		m_strLogDir[l]=0;
+		m_max_t_out=0;
+		return E_SUCCESS;
+	}
+
+SINT32 CADataRetentionLog::openLogFile(UINT32 time)
+	{
 		m_pLogFile=new CADataRetentionLogFile();
-		return m_pLogFile->openLog(strDir,time(NULL),m_pPublicEncryptionKey);
+		if(m_pLogFile->openLog(m_strLogDir,time,m_pPublicEncryptionKey)!=E_SUCCESS)
+			return E_UNKNOWN;
+		m_max_t_out=m_pLogFile->getMaxLogTime();
+		return E_SUCCESS;
+	}
+
+SINT32 CADataRetentionLog::closeLog()
+	{
+		if(m_pLogFile!=NULL)
+			{
+				m_pLogFile->closeLog();
+				delete m_pLogFile;
+				m_pLogFile=NULL;
+			}
+		return E_SUCCESS;
 	}
 
 SINT32 CADataRetentionLog::log(t_dataretentionLogEntry* logEntry)
 	{
+		if(ntohl(logEntry->t_out)>m_max_t_out)
+			{
+				if(m_pLogFile!=NULL)
+					{
+						m_pLogFile->closeLog();
+						delete m_pLogFile;
+						m_pLogFile=NULL;
+					}
+				if(openLogFile(ntohl(logEntry->t_out))!=E_SUCCESS)
+					return E_UNKNOWN;
+			}
 		return m_pLogFile->log(logEntry);
 	}
 #endif //DATA_RETENTION_LOG
