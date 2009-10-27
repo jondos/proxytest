901e847dc64c411b66b47cb6fde8dd8c4adf6336
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2009-10-27 13:13:59 +0000

    Auto generate through doAutoReconf.sh

--- doAutoReconf.sh
+++ doAutoReconf.sh
@@ -4,6 +4,6 @@
 rm missing aclocal.m4 configure config.h.in
 autoreconf -i -f -v -W all
 dos2unix -a -d -o configure
-svn commit -m "Auto generate through doAutoReconf.sh" -f missing aclocal.m4 Makefile.in config.h.in popt/Makefile.in xml/Makefile.in trio/Makefile.in tre/Makefile.in TypeA/Makefile.in TypeB/Makefile.in gcm/Makefile.in
+svn commit -m "Auto generate through doAutoReconf.sh" --cl missing aclocal.m4 Makefile.in config.h.in popt/Makefile.in xml/Makefile.in trio/Makefile.in tre/Makefile.in TypeA/Makefile.in TypeB/Makefile.in gcm/Makefile.in
 
 svn commit -m "Auto generate through doAutoReconf.sh"
