67e7bcb03dfb4550ff39a1282f6861cdc4065b8b
rolf <rolf@db673304-ec3e-4e33-90c0-be66232372a4>
2010-04-29 18:21:10 +0000

    - merged with stable - compatibility fix for openssl < 1.0.0

--- CATLSClientSocket.cpp
+++ CATLSClientSocket.cpp
@@ -35,8 +35,7 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMA
 CATLSClientSocket::CATLSClientSocket()
 	{
 		m_bConnectedTLS = false;
-		const SSL_METHOD* meth= TLSv1_client_method();
-		m_pCtx = SSL_CTX_new( meth );
+		m_pCtx = SSL_CTX_new( TLSv1_client_method() );
 #ifdef SSL_OP_NO_TICKET
 		// disable buggy TLS client extensions, as otherwise we will get no connection to a Java TLS server; the bug is fixes in OpenSSL > 0.9.8g
 		SSL_CTX_set_options(m_pCtx, SSL_OP_ALL|SSL_OP_NO_TICKET);
