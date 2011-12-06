e17523badd72cffa62fff4507666f844dc2e044d
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2011-12-06 12:21:01 +0000

    Auto generate through doAutoReconf.sh

--- doAutoReconf.sh
+++ doAutoReconf.sh
@@ -2,7 +2,7 @@
 #Use this file if you have changed anything related to autoconf/automake
 #Please use only the newest version of autoconf/automake
 rm missing aclocal.m4 configure config.h.in
-autoreconf -i -f -v -W all
+autoreconf -i -f -v -W all -I/usr/share/aclocal
 dos2unix -a -f -d -o configure
 svn commit -m "Auto generate through doAutoReconf.sh" --cl missing aclocal.m4 Makefile.in config.h.in popt/Makefile.in xml/Makefile.in trio/Makefile.in tre/Makefile.in TypeA/Makefile.in TypeB/Makefile.in gcm/Makefile.in test/Makefile.in wireshark/Makefile.in
 
