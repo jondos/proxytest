5eaa99b19af4e84cd2ef5c47ad69117bb69a37b7
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2017-05-10 08:01:15 +0000

    Auto generate through doAutoReconf.sh

--- proxytest/trunk/doAutoReconf.sh
+++ proxytest/trunk/doAutoReconf.sh
@@ -4,6 +4,11 @@
 rm missing aclocal.m4 configure config.h.in
 autoreconf -i -f -v -W all -I/usr/share/aclocal
 dos2unix -ascii -f -o configure
+
+if [ -z "$1" ] ; then
+
 svn commit -m "Auto generate through doAutoReconf.sh" --cl missing aclocal.m4 Makefile.in config.h.in popt/Makefile.in xml/Makefile.in trio/Makefile.in tre/Makefile.in TypeA/Makefile.in TypeB/Makefile.in gcm/Makefile.in test/Makefile.in wireshark/Makefile.in
 
 svn commit -m "Auto generate through doAutoReconf.sh"
+
+fi
