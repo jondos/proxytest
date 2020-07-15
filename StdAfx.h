3f617bddf61a6390d851bd19872335eccad9000f
sk13 <sk13@db673304-ec3e-4e33-90c0-be66232372a4>
2020-07-15 07:46:57 +0000

    Auto generate through doAutoReconf.sh

--- StdAfx.h
+++ StdAfx.h
@@ -104,8 +104,8 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMA
 //#define SSL_HACK //???
 
 //#define ANON_DEBUG_MODE //if defined this Mix operates in AN.ON protocol debug mode, which means that a lot of things are logged which should not be logged in productive mode. So NEVER enable this in productive environments!
-//#define NO_ENCRYPTION //!!!WARNING!! if defined, no encryption will happen! Only use in BSL-4 laboratory!
-//#define MUXSOCKET_CIPHER_NO_ENCRYPTION //!!!WARNING!! if defined, no encryption on the MuxSocket will happen! Only use in BSL-4 laboratory!
+#define NO_ENCRYPTION //!!!WARNING!! if defined, no encryption will happen! Only use in BSL-4 laboratory!
+#define MUXSOCKET_CIPHER_NO_ENCRYPTION //!!!WARNING!! if defined, no encryption on the MuxSocket will happen! Only use in BSL-4 laboratory!
 //#define FAST_PROCESSING //make all polling as fast as possible, may lead to very high processor utilisation in case of low traffic/workload; usefull for performance measurements
 //#define MULTI_THREADED_PACKET_PROCESSING //utilise multiple threads for packet processing, which could lead to a performacne improvement on multi core machines
 //#define __BUILD_AS_SHADOW_PLUGIN__ //define if you want to compile the Mix as a plugin for the Shadow simulator
